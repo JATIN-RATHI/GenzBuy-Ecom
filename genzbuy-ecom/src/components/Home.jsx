eaa99e80a3bcb065e310c1a3d046c376f0e60a80
vedant ujjwal <143152407+VedantUjjwal@users.noreply.github.com>
2025-06-27 15:21:32 +0530

    added cards carousel

--- genzbuy-ecom/src/components/Home.jsx
+++ genzbuy-ecom/src/components/Home.jsx
@@ -1,26 +1,225 @@
 import React from "react";
-import logo from "../assets/logo2.png";
-import logo3 from "../assets/logo3.png";
-import "../components/Home.css"; 
+import "../components/Home.css";
+import { imageBanner, cardimg, trendimg } from "../components/imgColl.jsx";
 
 const Home = () => {
-return (
-    // <div className="container">
-        <div className="home">
-            <div className="navbar">
-               <img src={logo3} alt="Logo" className="logo"/>
-                <div className="nav-links">
-                    <a href="/">Home</a>
-                    <a href="/products">Products</a>
-                    <a href="/about">About Us</a>
-                    <a href="/contact">Contact</a>
+  return (
+    <>
+      <div className="home">
+        <div className="navbar">
+          <div className="nav-links">
+            <img src="/images/wrogn-logo.png" alt="Logo" className="logo" />
+            <a href="/">EOSS</a>
+            <a href="/products">EXCLUSIVE</a>
+            <a href="/about">ALL PRODUCTS</a>
+            <a href="/contact">TOPWEAR</a>
+            <a href="/contact">BOTTOMWEAR</a>
+            <a href="/contact">FOOTWEAR</a>
+            <a href="/contact">FRESH ARRIVALS</a>
+            <a href="/contact">ESSENTIALS</a>
+          </div>
+          <div className="btn-links">
+            <form className="searchbar d-flex" role="search">
+              <input className="searchbar form-control me-2" type="search" placeholder="SEARCH" aria-label="Search" />
+            </form>
+            <img src="./images/icons8-account.gif" alt="User Icon" className="user-icon" />
+            <img src="./images/icons8-shopping-bag.gif" alt="Cart Icon" className="cart-icon" />
+          </div>
+        </div>
+        {/* carousel for banners */}
+        <div id="carouselExampleAutoplaying" className="carousel slide p-1" data-bs-ride="carousel">
+          <div className="carousel-inner rounded-4">
+            {imageBanner.map((item, index) => (
+              <div
+                key={item.id}
+                className={`carousel-item ${index === 0 ? "active" : ""}`}
+              >
+                <img src={item.url} className="d-block w-100" alt={item.name} />
+              </div>
+            ))}
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      {/* card carousel */}
+      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+        <div className="carousel-inner">
+          {cardimg.map((item, index) => (
+            <div
+              key={item.id}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <img src={item.url} className="d-block w-100" alt={item.name} />
+            </div>
+          ))}
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      {/* <div className="categories p-3">
+        <h1 className="head2"> TRENDING CATEGORIES </h1>
+        <div className="cat-party d-flex justify-content-around">
+          <img src="" className="border border-2 border-black rounded-4 float-start "alt="..."style={{ width: "650px", height: "400px" }}/>
+          <img src={img32}className="border border-2 border-black rounded-4 float-end "alt="..."style={{ width: "650px", height: "400px" }} />
+        </div>
+        <div className="cat-all d-flex justify-content-around p-3">
+          <img src="" className="border border-2 border-black rounded-4 float-start d-block"alt="..."style={{ width: "450px", height: "550px" }}/>
+          <img src="" className="border border-2 border-black rounded-4 float-start d-block"alt="..."style={{ width: "450px", height: "550px" }}/>
+          <img src="" className="border border-2 border-black rounded-4 float-start d-block"alt="..."style={{ width: "450px", height: "550px" }}/>
+        </div>
+      </div> */}
+      {/* <div className="store">
+        <div className="categories p-3">
+          <div className="cat-party d-flex justify-content-around">
+            <p className="title">GENZBUY STORE.<br/><span className="titl-bold"> RIGHT PLACE.</span><br/><span className="titl-para"> For The Latest Fashion Trends.</span></p> 
+            <img src="" className="rounded-4 float-start rounded-4 float-end border border-2 border-black" alt="..."style={{ width: "650px", height: "400px" }} />
+          </div>
+          <div className="store-img d-flex justify-content-around m-3">
+          <img src="" className="rounded-4 float-end border border-2 border-black"alt="..."style={{ width: "600px", height: "450px" }}/>
+          <img src="" className="rounded-4 float-end border border-2 border-black" alt="..."style={{ width: "400px", height: "450px" }}/>
+          <img src="" className="rounded-4 float-end border border-2 border-black"alt="..."style={{ width: "400px", height: "450px" }}/>
+          </div>
+        </div>
+      </div> */}
+      {/* <div className="footer">
+        <footer className="bg-dark text-white pt-5 pb-4">
+          <div className="container text-center text-md-start">
+            <div className="row text-center text-md-start">
+              <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h5 className="text-uppercase mb-4 font-weight-bold text-white">
+                  GenzBuy
+                </h5>
+                <p className="text-secondary">
+                  Discover the latest trends and timeless styles. Your ultimate
+                  fashion destination.
+                </p>
+              </div>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 className="text-uppercase mb-4 font-weight-bold text-white">
+                  Quick Links
+                </h5>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    About Us
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    Shop All
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    Shop All
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    New Arrivals
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    Sale
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <a href="#" className="text-white text-decoration-none">
+                    Blog
+                  </a>
+                </p>
+              </div>
+
+              <div className="col-md-5 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h5 className="text-uppercase mb-4 font-weight-bold text-white">
+                  Connect With Us
+                </h5>
+                <p className="mb-1">
+                  <i className="fas fa-envelope me-2"></i> Email:{" "}
+                  <a
+                    href="mailto:info@genzbuy.com"
+                    className="text-white text-decoration-none"
+                  >
+                    info@genzbuy.com
+                  </a>
+                </p>
+                <p className="mb-1">
+                  <i className="fas fa-phone me-2"></i> Phone:{" "}
+                  <a
+                    href="tel:+1234567890"
+                    className="text-white text-decoration-none"
+                  >
+                    +1 (234) 567-890
+                  </a>
+                </p>
+                <div className="mt-4">
+                  <ul className="list-unstyled list-inline">
+                    <li className="list-inline-item me-3">
+                      <a
+                        href="#"
+                        className="btn-floating btn-sm text-white"
+                        style={{ fontSize: "23px" }}
+                      >
+                        <i className="fab fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li className="list-inline-item me-3">
+                      <a
+                        href="#"
+                        className="btn-floating btn-sm text-white"
+                        style={{ fontSize: "23px" }}
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li className="list-inline-item">
+                      <a
+                        href="#"
+                        className="btn-floating btn-sm text-white"
+                        style={{ fontSize: "23px" }}
+                      >
+                        <i className="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <div className="btn-links">
-                    <a href="/login">Login</a>
-                    <a href="/signup">Sign Up</a>
-                </div>
-           </div>
-        </div>      
-    // </div>
-)};
+              </div>
+            </div>
+            <hr className="mb-4" />
+            <div className="row d-flex justify-content-center">
+              <div className="col-md-8 col-lg-8">
+                <p className="text-center text-secondary">
+                  &copy; 2024 GenzBuy. All rights reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div> */}
+    </>
+  );
+};
+
 export default Home;
---
+++ genzbuy-ecom/src/components/imgColl.jsx
@@ -0,0 +1,68 @@
+
+// best sellers image  
+const bestSellerimg = [
+  { id: 7, name: "bscargo", url: "/images/bscargo.webp" },
+  { id: 8, name: "bsjeans", url: "/images/bsjeans.webp" },
+  { id: 9, name: "bsshirt", url: "/images/bsshirt.webp" },
+];
+
+// images for carousel 
+const carouselimg = [
+  { id: 10, name: "carousel1", url: "/images/carousel1.webp" },
+  { id: 11, name: "carousel2", url: "/images/carousel2.webp" },
+  { id: 12, name: "carousel3", url: "/images/carousel3.webp" },
+  { id: 13, name: "carousel4", url: "/images/carousel4.webp" },
+  { id: 14, name: "carousel5", url: "/images/carousel5.webp" },
+  { id: 15, name: "carousel6", url: "/images/carousel6.webp" },
+  { id: 16, name: "carousel7", url: "/images/carousel7.webp" },
+];
+
+// images for cards
+const cardimg = [
+    { id: 1, name: "crd1", url: "/images/crd1.webp" },
+    { id: 2, name: "crd2", url: "/images/crd2.webp" },
+    { id: 3, name: "crd3", url: "/images/crd3.webp" },
+    { id: 4, name: "crd4", url: "/images/crd4.webp" },
+    { id: 5, name: "crd5", url: "/images/crd5.webp" },
+    { id: 6, name: "crd6", url: "/images/crd6.webp" },
+    { id: 7, name: "crd7", url: "/images/crd7.webp" },
+    { id: 8, name: "crd8", url: "/images/crd8.webp" },
+];
+
+// images for season card
+const seasoncardimg = [
+    { id: 25, name: "ssncard1", url: "/images/ssncard1.webp" },
+    { id: 26, name: "ssncard2", url: "/images/ssncard2.webp" },
+    { id: 27, name: "ssncard3", url: "/images/ssncard3.webp" },
+    { id: 28, name: "ssncard4", url: "/images/ssncard4.webp" },
+    { id: 29, name: "ssncard5", url: "/images/ssncard5.webp" },
+    { id: 30, name: "ssncard6", url: "/images/ssncard6.webp" },
+    { id: 31, name: "ssncard7", url: "/images/ssncard7.webp" },
+];
+
+// images for trending category
+const trendimg = [
+    { id: 32, name: "trendCatg1", url: "/images/trendCatg1.webp" },
+    { id: 33, name: "trendCatg2", url: "/images/trendCatg2.webp" },
+    { id: 34, name: "trendCatg3", url: "/images/trendCatg3.webp" },
+    { id: 35, name: "trendCatg4", url: "/images/trendCatg4.webp" },
+    { id: 36, name: "trendCatg5", url: "/images/trendCatg5.webp" },
+];
+
+// wallpaper of wrogn
+const wwimg = [
+    { id: 37, name: "ww1", url: "/images/ww1.webp" },
+    { id: 38, name: "ww2", url: "/images/ww2.webp" },
+    { id: 39, name: "ww3", url: "/images/ww3.webp" },
+   
+];
+
+const imageBanner = [
+  { id: 1, name: "ban1", url: "/images/ban1.webp" },
+  { id: 2, name: "ban2", url: "/images/ban2.webp" },
+  { id: 3, name: "ban3", url: "/images/ban3.webp" },
+  { id: 4, name: "ban4", url: "/images/ban4.webp" },
+  { id: 5, name: "ban5", url: "/images/ban5.webp" },
+  { id: 6, name: "ban6", url: "/images/ban6.webp" },
+];
+export { cardimg, imageBanner, carouselimg, bestSellerimg, seasoncardimg, trendimg, wwimg};
